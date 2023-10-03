d985e945eb00906a34cdb13319699b8b2a6ececa
lasya <lasya@LaSyasONI.bbrouter>
2023-10-03 21:45:11 +0530

    firstcommit

---
+++ Program/src/Operators.java
@@ -0,0 +1,17 @@
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=20;
+		int b=10;
+		int c=15;
+		++a;
+		b--;
+		int d=a++ + b + --c;
+		System.out.println(d);
+		
+
+	}
+
+}
